6d387ecd6b96aeca8b2ef5b07c81fab95d4827cb
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-09-26 01:29:46 +0100

    Progress 2

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 // This package (qota - Qamarian iOTA ) implements the iota data type (a data type which
 // is like the Golang iota). When you create a new qota, it is automatically assigned a
 // value of 0. However, you could grow the qota by 1, with every call to its method
-// Grow ().
+// Grow (). But once a qota reaches the value of 255, it would stop growing.
 //
 // 	someQota := qota.New () // This is how qotas should always be created.
 // 	someQota.Value () // Returns 0.
