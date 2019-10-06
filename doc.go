cce9cdb4ac810b321e7d1b3b148599b2c349694a
Ibrahim Oladipupo Qamardeen <kensharval@gmail.com>
2019-10-06 01:29:37 +0100

    Progress 5

--- doc.go
+++ doc.go
@@ -1,9 +1,9 @@
-// Package (qota - Qamarian iota) implements the iota data type (a data type which works
-// like the Golang "iota").
+// Package qota (Qamarian iota) implements the iota data type -- a data type which works
+// like the Golang "iota".
 //
-// 	someQota := qota.New () // This is how qotas should always be created.
-// 	someQota.Value () // Returns 0.
-// 	someQota.Value () // Returns 1.
-// 	someQota.Value () // Returns 2.
+// 	i := qota.New () // This is how qotas should always be created.
+// 	i.Value () // Returns 0.
+// 	i.Value () // Returns 1.
+// 	i.Value () // Returns 2.
 //
 package qota
